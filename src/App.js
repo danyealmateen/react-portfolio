7b3bc4c33e7dffac00abd69c1c46694695f158b2
DanyMateen <danyeal.mateen@hotmail.com>
2022-11-07 14:16:56 +0100

    second push

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import Services from "./components/Services/Services";
 
 function App() {
   return (
     <div>
       <Header />
+      <Services />
     </div>
   );
 }
---
+++ src/components/Services/Services.js
@@ -0,0 +1,15 @@
+import "./Services.css";
+
+function Services() {
+  return (
+    <div>
+      <h1 className="myServicesTitle">My services</h1>
+      <div className="grid-container">
+        <div className="grid-item">Lorem ipsum dolor sit amet.</div>
+        <div className="grid-item"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Services;
