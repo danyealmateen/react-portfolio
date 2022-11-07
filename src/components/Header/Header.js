e5340d87466faaa2dfc4de76be24f6bd746194e9
DanyMateen <danyeal.mateen@hotmail.com>
2022-11-07 10:52:03 +0100

    header updated

---
+++ src/components/Header/Header.js
@@ -0,0 +1,26 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="title">
+        <h1>
+          <span className="devName">I'm Danyeal Mateen</span>
+        </h1>
+        <h1>
+          <span className="devTitle">Front-end Developer</span>
+        </h1>
+        <p className="textContent">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. At
+          necessitatibus pariatur odit error eius minima, ut illo laudantium qui
+          eveniet? Sunt, reprehenderit? Voluptate quos fuga earum itaque
+          repudiandae suscipit provident odit doloribus unde reiciendis. Alias
+          maiores mollitia maxime, vero, delectus excepturi totam expedita,
+          aliquid eligendi rerum omnis laudantium nesciunt sint.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
