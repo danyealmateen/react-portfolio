c22e2686b95678290a4d93357618d0623fe65de9
DanyMateen <danyeal.mateen@hotmail.com>
2022-11-08 10:27:56 +0100

    added Dropdown and Element components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Services from "./components/Services/Services";
 import Skills from "./components/Skills/Skills";
+import { Dropdown } from "./components/Dropdown/Dropdown";
+import { Element } from "./components/Element/Element";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Header />
       <Services />
       <Skills />
+      <Dropdown />
+      <Element />
     </div>
   );
 }
